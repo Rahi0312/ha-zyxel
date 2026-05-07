a7abfbc8d52dd4734691ccadeebaf109ac09fc91
Rahi0312 <133817715+Rahi0312@users.noreply.github.com>
2026-05-07 12:13:23 +0200

    Update const.py
    default scan interval changed to 300s

--- custom_components/ha_zyxel/const.py
+++ custom_components/ha_zyxel/const.py
@@ -2,7 +2,7 @@ DOMAIN = "ha_zyxel"
 DEFAULT_NAME = "Zyxel Device"
 DEFAULT_HOST = "https://192.168.1.1"
 DEFAULT_USERNAME = "admin"
-DEFAULT_SCAN_INTERVAL = 30
+DEFAULT_SCAN_INTERVAL = 300
 
 CONF_HOST = "host"
 CONF_USERNAME = "username"
